1944940232a3cc70e71698c57b8400ee475364ea
miyujie <miyujie2015@ipalfish.com>
2021-09-13 16:04:48 +0800

    hello vue-cli

--- packages/@vue/cli/lib/create.js
+++ packages/@vue/cli/lib/create.js
@@ -8,6 +8,7 @@ const { chalk, error, stopSpinner, exit } = require('@vue/cli-shared-utils')
 const validateProjectName = require('validate-npm-package-name')
 
 async function create (projectName, options) {
+  // 是不是设置了代理
   if (options.proxy) {
     process.env.HTTP_PROXY = options.proxy
   }
@@ -18,6 +19,7 @@ async function create (projectName, options) {
   const targetDir = path.resolve(cwd, projectName || '.')
 
   const result = validateProjectName(name)
+  // 检验项目名称是否符合npm包名的规范
   if (!result.validForNewPackages) {
     console.error(chalk.red(`Invalid project name: "${name}"`))
     result.errors && result.errors.forEach(err => {
@@ -29,12 +31,16 @@ async function create (projectName, options) {
     exit(1)
   }
 
+  // 判断当前路径存不存在 and 有没有选择强制merge
   if (fs.existsSync(targetDir) && !options.merge) {
+    // 如果选择了force 会移除当前目录
     if (options.force) {
       await fs.remove(targetDir)
     } else {
       await clearConsole()
+      // 判断是否在当前目录内
       if (inCurrent) {
+        // 二次确认
         const { ok } = await inquirer.prompt([
           {
             name: 'ok',
@@ -46,6 +52,7 @@ async function create (projectName, options) {
           return
         }
       } else {
+        // 询问是进行 覆盖 合并 或者取消
         const { action } = await inquirer.prompt([
           {
             name: 'action',
@@ -68,6 +75,7 @@ async function create (projectName, options) {
     }
   }
 
+  // 开始创建模板
   const creator = new Creator(name, targetDir, getPromptModules())
   await creator.create(options)
 }
